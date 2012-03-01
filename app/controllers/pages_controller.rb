4e536a701a9fad1b68199e5edd8025e2399ca44d
Mattias Ottosson <himself@mattiasottosson.se>
2012-03-02 00:52:34 +0100

    Bumped cache time to 30 secs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def index
     last_modified = File.mtime("#{Rails.root}/app/views/pages/index.html.erb")
     fresh_when last_modified: last_modified , public: true, etag: last_modified
-    expires_in 10.seconds, public: true
+    expires_in 30.seconds, public: true
   end
   
 end
