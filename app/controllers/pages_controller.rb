0197f0f794ec93df78f898e476be05dbfde3fc19
Mattias Ottosson <himself@mattiasottosson.se>
2012-03-01 18:04:32 +0100

    Added index action and template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+  
+  def index
+    last_modified = File.mtime("#{Rails.root}/app/views/pages/index.html.erb")
+    fresh_when last_modified: last_modified , public: true, etag: last_modified
+    expires_in 10.seconds, public: true
+  end
+  
 end
